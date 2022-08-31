e3e58a86e2c97ec0e4481b530570f48a78dcc29a
Kartikey Aggarwal <71753524+kartikeyaggarwal@users.noreply.github.com>
2022-08-31 21:36:38 +0530

    Create printer.go

---
+++ printer.go
@@ -0,0 +1,30 @@
+type Request struct {
+	BgImgPath string
+	FontPath  string
+	FontSize  float64
+	Text      string
+}
+
+func TextOnImg(request Request) (image.Image, error) {
+	bgImage, err := gg.LoadImage(request.BgImgPath)
+	if err != nil {
+		return nil, err
+	}
+	imgWidth := bgImage.Bounds().Dx()
+	imgHeight := bgImage.Bounds().Dy()
+
+	dc := gg.NewContext(imgWidth, imgHeight)
+	dc.DrawImage(bgImage, 0, 0)
+
+	if err := dc.LoadFontFace(request.FontPath, request.FontSize); err != nil {
+		return nil, err
+	}
+
+	x := float64(imgWidth / 2)
+	y := float64((imgHeight / 2) - 80)
+	maxWidth := float64(imgWidth) - 60.0
+	dc.SetColor(color.White)
+	dc.DrawStringWrapped(request.Text, x, y, 0.5, 0.5, maxWidth, 1.5, gg.AlignCenter)
+
+	return dc.Image(), nil
+}
