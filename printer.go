06b7d62ba094f389ede8a9024dc78436cded29c0
Kartikey Aggarwal <71753524+kartikeyaggarwal@users.noreply.github.com>
2022-09-01 23:38:58 +0530

    Add files via upload

--- printer.go
+++ printer.go
@@ -7,8 +7,13 @@ import (
 	"github.com/fogleman/gg"
 )
 
-//TextOnImg given a path to an image, a font & size and a text it will return an Image with the
-//given text printed in the ~middle of the image
+type Request struct {
+	BgImgPath string
+	FontPath  string
+	FontSize  float64
+	Text      string
+}
+
 func TextOnImg(request Request) (image.Image, error) {
 	bgImage, err := gg.LoadImage(request.BgImgPath)
 	if err != nil {
@@ -25,21 +30,10 @@ func TextOnImg(request Request) (image.Image, error) {
 	}
 
 	x := float64(imgWidth / 2)
-	signatureOff := 80
-	y := float64((imgHeight / 2) - signatureOff)
-	textMargin := 60.0
-	maxWidth := float64(imgWidth) - textMargin
-	dc.SetColor(color.Black)
-	dc.DrawStringWrapped(request.Text, x+3, y+3, 0.5, 0.5, maxWidth, 1.5, gg.AlignCenter)
+	y := float64((imgHeight / 2) - 80)
+	maxWidth := float64(imgWidth) - 60.0
 	dc.SetColor(color.White)
 	dc.DrawStringWrapped(request.Text, x, y, 0.5, 0.5, maxWidth, 1.5, gg.AlignCenter)
 
 	return dc.Image(), nil
-}
-
-func Save(img image.Image, path string) error {
-	if err := gg.SavePNG(path, img); err != nil {
-		return err
-	}
-	return nil
 }
